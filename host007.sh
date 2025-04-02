8a5e18dfae06fccfb774fe6b79fe29e40213a7d4
Zero_Code <129968635+Mathews324@users.noreply.github.com>
2025-04-02 14:46:32 -0300

    Add files via upload

---
+++ host007.sh
@@ -0,0 +1,6 @@
+apt install wget curl 
+curl -SsL https://playit-cloud.github.io/ppa/key.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/playit.gpg >/dev/null
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/playit.gpg] https://playit-cloud.github.io/ppa/data ./" | sudo tee /etc/apt/sources.list.d/playit-cloud.list
+apt update
+apt install playit
+echo "o programa foi instalado para usar digite playit"
