29ca0472f712589b1216f1dac7ff127018441940
Zero_Code <129968635+Mathews324@users.noreply.github.com>
2025-04-02 14:48:29 -0300

    Delete host007.sh

--- host007.sh
+++
@@ -1,6 +0,0 @@
-apt install wget curl 
-curl -SsL https://playit-cloud.github.io/ppa/key.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/playit.gpg >/dev/null
-echo "deb [signed-by=/etc/apt/trusted.gpg.d/playit.gpg] https://playit-cloud.github.io/ppa/data ./" | sudo tee /etc/apt/sources.list.d/playit-cloud.list
-apt update
-apt install playit
-echo "o programa foi instalado para usar digite playit"
